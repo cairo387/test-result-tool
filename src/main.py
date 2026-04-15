b6b47adb0f1248e255a3febb6876c53bf655d422
cairo387 <mifuneri@outlook.jp>
2026-04-15 20:54:28 +0900

    Add CSV reading functionality

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
+import csv
+
+#csvファイルを読み込む関数を定義
+def read_csv(file_path):
+    #file_pathで指定されたファイルをUTF-8で開き、以降fとして使う
+    with open(file_path, encoding="utf-8") as f:
+        #csvの内容を辞書形式で読めるように変換
+        reader = csv.DictReader(f)
+        #CSVの1行ずつを順番に取り出して表示
+        for row in reader:
+            print(row)
+
+
 def main():
-    print("Hello, test-result-tool")
+    file_path = "data/sample.csv"
+    read_csv(file_path)
 
 
 if __name__ == "__main__":
